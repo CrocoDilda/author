502b268f6787a7d5f8ad916b4d8f1f5d1c4cf5eb
CrocoDilda <118596934+CrocoDilda@users.noreply.github.com>
2024-07-19 13:15:37 +0300

    Update app.js
    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const author = document.querySelector('[data-author]')
 author.innerHTML = `
-<div id="intro-dm" class="author--wrapper" style="	
+<div id="intro-dm" class="author--wrapper" style="
+	position: absolute;
+	left: 15px;
+	bottom: 10px;
 	max-width: 300px;
 	display: grid;
 	grid-auto-flow: column;
@@ -27,9 +30,9 @@ author.innerHTML = `
 		width: 24px;">
 			<svg class="author--icon" viewBox="0 0 24 24">
 				<g>
-					<circle cx="12" cy="12"  r="12">
+					<circle fill="#039BE5" cx="12" cy="12"  r="12">
 					</circle>
-					<path d="m5.491 11.74 11.57-4.461c.537-.194 1.006.131.832.943l.001-.001-1.97 9.281c-.146.658-.537.818-1.084.508l-3-2.211-1.447 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.121l-6.871 4.326-2.962-.924c-.643-.204-.657-.643.136-.953z" fill="#fff"></path>
+					<path fill="#fff" d="m5.491 11.74 11.57-4.461c.537-.194 1.006.131.832.943l.001-.001-1.97 9.281c-.146.658-.537.818-1.084.508l-3-2.211-1.447 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.121l-6.871 4.326-2.962-.924c-.643-.204-.657-.643.136-.953z"></path>
 				</g>
 			</svg>
 		</a>
